3cfd87dfd4a30b4efc5d4cab07cc692077ecdb53
Prashanth Reddy <prashu103096@gmail.com>
2016-10-03 21:39:49 +0530

    lastone

--- imgdown.py
+++ imgdown.py
@@ -9,9 +9,12 @@ page = page_htm.read()
 #regex part
 patt_img=re.compile('<img[^>]*src="([^">]+)"',re.IGNORECASE)
 result=re.findall(patt_img,page)
-
+test_pat=re.compile('https://.*')
 #writing to the file 
 f=open("output.txt","w")
 for i in result:
-	f.write(url+i+"\n")
+	if re.match(test_pat,i):
+		f.write(i+"\n")
+	else:
+		f.write(url+i+"\n")
 f.close()
